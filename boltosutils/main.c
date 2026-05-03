ccab97987df5ab39f5ab63ece268dbcf9ddd088b
Noah Wooten <noah.wooten@icloud.com>
2026-05-03 12:34:27 +0800

    Initial Commit

---
+++ boltosutils/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  boltosutils
+//
+//  Created by Noah Wooten on 5/3/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
