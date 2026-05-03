bcdcd26f49e91f8abd6b4e69e117dc67bf3dd07e
Noah Wooten <noah.wooten@icloud.com>
2026-05-03 13:26:49 +0800

    superficial change

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ int main(int argc, char** argv) {
         Version(argc, argv);
     } else if (!strcmp(argv[1], "resource")) {
         //Resource(argc, argv);
+        
     }
     
     return 0;
